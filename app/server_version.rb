064e2d7913831957e9dc386455183201abf48bae
王炜堃 <wangwk@dehuinet.comm>
2018-11-22 10:51:59 +0800

    emm

--- app/server_version.rb
+++ app/server_version.rb
@@ -1,8 +1,8 @@
 
+#数据存储
 DataBase._redis_.set("testkey","testvalue")
-# 数据存储
-# puts DataBase._redis_.keys("*") 
-# puts DataBase._redis_.get("testkey")
+puts DataBase._redis_.keys("*") 
+puts DataBase._redis_.get("testkey")
 EventManage::register_event(Proc.new do |r|
     Net::send_with_recv("version:0.0.1\0",r)
 end,{
