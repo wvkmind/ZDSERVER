7768550886cd2e3587926f887749f53efa1535f8
王炜堃 <wangwk@dehuinet.comm>
2018-11-23 09:41:26 +0800

    null

--- app/server_version.rb
+++ app/server_version.rb
@@ -8,7 +8,7 @@ end,{
 
 
 EventManage::register_event(Proc.new do |r|
-    Net::send_with_recv("#{DataBase._redis_.get("my_id")}\0",r)
+    Net::send_with_recv("#{r[:name]}:#{DataBase._redis_.get("my_id")}\0",r)
 end,{
     :name=>:get_my_id,:type=>:normal
 })
