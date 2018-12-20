3489df50b5bebab0b211844c718030a60f6fa697
wvkmind <wangwk@dehuinet.comm>
2018-12-20 15:08:13 +0800

    add transaction

--- app/account.rb
+++ app/account.rb
@@ -1,31 +1,32 @@
 Net::Connector.registergate('register',-> params,gete do
     begin
-        user = User.new
-        user[:account]=params['account']
-        ps = AccountHelper.generate_password(params['password'])
-        user[:hashed_password]=ps[:hash_password].to_s
-        user[:salt]=ps[:salt].to_s
-        user[:status] = 0
-        raise 'Must have passwrod.' if user[:hashed_password].nil?
-        user[:status]=params['status'] unless params['status'].nil?
-        raise Exception.new('Need role type.') if params['type'].nil?
-        user[:type]=params['type'].to_i
-        user[:tra_rate]=params['tra_rate'].to_i
-        user[:phy_str_rate]=params['phy_str_rate'].to_i
-        user[:exp_rate]=params['exp_rate'].to_i
-        raise Exception.new("Hack?") if user[:tra_rate] + user[:phy_str_rate] + user[:exp_rate] > 5
-        user[:level]=1
-        user[:zhanyang]=0
-        user[:buliang]=0
-        user.save
-        gete.send(user.to_h,params)
+        User.transaction do
+            user = User.new
+            user[:account]=params['account']
+            ps = AccountHelper.generate_password(params['password'])
+            user[:hashed_password]=ps[:hash_password].to_s
+            user[:salt]=ps[:salt].to_s
+            user[:status] = 0
+            raise 'Must have passwrod.' if user[:hashed_password].nil?
+            user[:status]=params['status'] unless params['status'].nil?
+            raise Exception.new('Need role type.') if params['type'].nil?
+            user[:type]=params['type'].to_i
+            user[:tra_rate]=params['tra_rate'].to_i
+            user[:phy_str_rate]=params['phy_str_rate'].to_i
+            user[:exp_rate]=params['exp_rate'].to_i
+            raise Exception.new("Hack?") if user[:tra_rate] + user[:phy_str_rate] + user[:exp_rate] > 5
+            user[:level]=1
+            user[:zhanyang]=0
+            user[:buliang]=0
+            user.save
+            gete.send(user.to_h,params)
+        end
     rescue Exception => e
         gete.send({status: 1,error:e.message},params)
     end
 end)
 
 Net::Connector.registergate('login',-> params,gete do
-    puts "login"
     begin
         ret = AccountHelper::login params['account'], params['password']
         session = ret[:session]
