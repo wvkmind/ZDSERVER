a8fb169d55b30539fd6d184a33db0d30a4e4742a
wvkmind <wangwk@dehuinet.comm>
2019-01-30 12:26:13 +0800

    add something

--- app/account.rb
+++ app/account.rb
@@ -1,22 +1,23 @@
 Net::Connector.registergate('register',-> params,gete do
     begin
         User.transaction do
-            raise 'Account exsit.' if(User.find_by_account params['account'])
+            raise '账号已存在' if(User.find_by_account params['account'])
+            raise '名字已存在' if(User.find_by_name params['name'])
             user = User.new
             user[:account]=params['account']
             ps = AccountHelper.generate_password(params['password'])
             user[:hashed_password]=ps[:hash_password].to_s
             user[:salt]=ps[:salt].to_s
             user[:status] = 0
-            raise 'Must have passwrod.' if user[:hashed_password].nil?
+            raise '密码呢' if user[:hashed_password].nil?
             user[:status]=params['status'] unless params['status'].nil?
-            raise Exception.new('Need role type.') if params['type'].nil?
+            raise Exception.new('没有选人呀') if params['type'].nil?
             user[:name]=params['user_name'].to_s
             user[:type]=params['type'].to_i
             user[:tra_rate]=params['tra_rate'].to_i
             user[:phy_str_rate]=params['phy_str_rate'].to_i
             user[:exp_rate]=params['exp_rate'].to_i
-            raise Exception.new("Hack?") if user[:tra_rate] + user[:phy_str_rate] + user[:exp_rate] > 5
+            raise Exception.new("你想干啥") if user[:tra_rate] + user[:phy_str_rate] + user[:exp_rate] > 5
             user[:level]=1
             user[:zhanyang]=0
             user[:buliang]=0
@@ -52,7 +53,7 @@ Net::Connector.registergate('login',-> params,gete do
                 params
             )
         else
-            raise Exception.new('Server is full.')
+            raise Exception.new('服务器好像满了')
         end
     rescue Exception => e
         Log.re(e)
---
+++ db/migrate/5_user_name_unique.rb
@@ -0,0 +1,5 @@
+class UserNameUnique < ActiveRecord::Migration[5.2]
+    def change
+        add_index :users, [:name], unique:true
+    end
+end
