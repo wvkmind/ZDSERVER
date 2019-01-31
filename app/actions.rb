5414c3f5c902e61c05ff61f3256d7c1acbc2db73
wvkmind <wangwk@dehuinet.comm>
2019-01-31 15:17:08 +0800

    fix bug

--- app/actions.rb
+++ app/actions.rb
@@ -16,7 +16,7 @@ Net::Connector.registerlogic('ac',-> params,my_node do
     begin
         user = User.get_user(params[:user_id])
         user.set_room_pos(params['ac_data'])
-        Room.send_data(params[:user_id],{cp_data:params['ac_data']}.merge({id: params[:user_id]}),params)
+        Room.send_data(params[:user_id],{ac_data:params['ac_data']}.merge({id: params[:user_id]}),params)
     rescue Exception => e
         Room.out_room(params[:user_id])
         my_node.send({status: 1,error:e.message},params)
