8fdfbe31430f3632f2eee8555286cea9cecb7698
wvkmind <wangwk@dehuinet.comm>
2019-02-18 18:39:55 +0800

    fix bug

--- app/map.rb
+++ app/map.rb
@@ -11,7 +11,8 @@ end)
 Net::Connector.registerlogic('pick',-> params,my_node do
     begin
         item = Room.pick_items(params[:user_id],params['pos'].to_i)
-        Room.send_data(params[:user_id],{status:0,items:map.get_items,user_id: params[:user_id],pick_pos:params['pos'].to_i,id:item.get_id},params) 
+        item_list = Room.item_list(params[:user_id])
+        Room.send_data(params[:user_id],{status:0,items:item_list,user_id: params[:user_id],pick_pos:params['pos'].to_i,id:item.get_id},params) 
     rescue Exception => e
         Log.re(e)
         my_node.send({status: 1,error:e.message},params)
@@ -21,7 +22,8 @@ end)
 Net::Connector.registerlogic('eat',-> params,my_node do
     begin
         if Room.eat(params[:user_id],params[:pos].to_i)
-            Room.send_data(params[:user_id],{status:0,eat_pos:params[:pos].to_i,items:map.get_items,user_id: params[:user_id]},params)
+            item_list = Room.item_list(params[:user_id])
+            Room.send_data(params[:user_id],{status:0,eat_pos:params[:pos].to_i,items:item_list,user_id: params[:user_id]},params)
         else
             my_node.send({status: 1},params)
         end
