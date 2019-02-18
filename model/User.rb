66aa7cce96bb926ed98dd57e260ec33c477a97a9
wvkmind <wangwk@dehuinet.comm>
2019-02-18 18:25:56 +0800

    fix bug

--- model/User.rb
+++ model/User.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	self.primary_key = "id"
 	self.inheritance_column = '_type'
 
-	has_many :packages, class_name: "Package"
+	has_many :packages
 
 	@@user_mem = {}
 	def to_h
--- model/package.rb
+++ model/package.rb
@@ -2,4 +2,5 @@ class Package < ActiveRecord::Base
     self.table_name = "packages"
 	self.primary_key = "id"
     self.inheritance_column = '_type'
+    belongs_to :user, class_name: "User", foreign_key: "package_id"
 end
