0ed28b32afdd384c0c2c73efb6c4d49328f37d3a
wvkmind <wangwk@dehuinet.comm>
2019-02-19 17:54:03 +0800

    fixbug

--- db/migrate/9_columns_fix.rb
+++ db/migrate/9_columns_fix.rb
@@ -3,7 +3,7 @@ class ColumnsFix < ActiveRecord::Migration[5.2]
         remove_column :users, :package_id
         remove_column :users, :tilizhi
         remove_column :users, :exp
-        add_column :users, :tilizhi, :integer , default: 100,
+        add_column :users, :tilizhi, :integer , default: 100
         add_column :users, :exp, :integer, default: 0
     end
 end
