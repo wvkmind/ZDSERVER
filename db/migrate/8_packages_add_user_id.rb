0962e20b3e985b864909618d723d633b47bf26db
wvkmind <wangwk@dehuinet.comm>
2019-02-19 17:52:00 +0800

    fixbug

--- db/migrate/8_packages_add_user_id.rb
+++ db/migrate/8_packages_add_user_id.rb
@@ -1,9 +1,5 @@
 class PackagesAddUserId < ActiveRecord::Migration[5.2]
     def change
-        remove_column :users, :package_id
-        remove_column :users, :tilizhi
-        remove_column :users, :exp
-        add_column :users, :tilizhi, :integer , default: 100
-        add_column :users, :exp, :integer, default: 0
+        add_column :packages, :user_id, :integer
     end
 end
--- db/migrate/9_columns_fix.rb
+++ db/migrate/9_columns_fix.rb
@@ -1,5 +1,9 @@
 class ColumnsFix < ActiveRecord::Migration[5.2]
     def change
-        
+        remove_column :users, :package_id
+        remove_column :users, :tilizhi
+        remove_column :users, :exp
+        add_column :users, :tilizhi, :integer , default: 100,
+        add_column :users, :exp, :integer, default: 0
     end
 end
