42818555f12f78f689e5809699bfa67fe2005e73
wvkmind <wangwk@dehuinet.comm>
2019-02-19 12:34:05 +0800

    fixbug

--- app/map.rb
+++ app/map.rb
@@ -7,13 +7,13 @@ end)
 Net::Connector.registerlogic('pick',-> params,my_node do
     item = Room.pick_items(params[:user_id],params['pos'].to_i)
     item_list = Room.item_list(params[:user_id])
-    Room.send_data(params[:user_id],{status:0,items:item_list,user_id: params[:user_id],pick_pos:params['pos'].to_i,id:item.get_id},params) 
+    Room.send_data(params[:user_id],{status:0,items:item_list,user_id: params[:user_id],pick_pos:params['pos'].to_i,id:item.get_id,type: item.type},params) 
 end)
 
 Net::Connector.registerlogic('eat',-> params,my_node do
     if Room.eat(params[:user_id],params[:pos].to_i)
         item_list = Room.item_list(params[:user_id])
-        Room.send_data(params[:user_id],{status:0,eat_pos:params[:pos].to_i,items:item_list,user_id: params[:user_id]},params)
+        Room.send_data(params[:user_id],{status:0,eat_pos:params[:pos].to_i,items:item_list,user_id: params[:user_id],id:item.get_id,type: item.type},params)
     else
         my_node.send({status: 1},params)
     end
